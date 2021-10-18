ccc6128fd076979b30fd4b0dec0b76270e45cd06
Tasnim Nahid <nnnahidt@gmail.com>
2021-10-18 10:17:41 +0600

    install firebase,react,bootstrap,change title

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDdMXlQni4PBlorMjmq8yVJan80TL3raY0",
+    authDomain: "kids-health-care.firebaseapp.com",
+    projectId: "kids-health-care",
+    storageBucket: "kids-health-care.appspot.com",
+    messagingSenderId: "229707147118",
+    appId: "1:229707147118:web:801ded7475ed195830837f"
+  };
+  export default firebaseConfig;
